e39e7a61bc80186518d2126b6030172aead3489a
mdavisprog <mdavisprog@gmail.com>
2022-01-29 14:57:27 -0800

    Added Button test suite and Pressed test case.

---
+++ Apps/Tests/Button.cpp
@@ -0,0 +1,64 @@
+/**
+
+MIT License
+
+Copyright (c) 2022 Mitchell Davis <mdavisprog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include "OctaneUI/OctaneUI.h"
+#include "TestSuite.h"
+
+namespace Tests
+{
+
+TEST_SUITE(Button,
+
+TEST_CASE(Press,
+{
+	const char* Json = "{\"Width\": 1280, \"Height\": 720, \"Body\": {\"Controls\": [{\"Type\": \"Button\", \"ID\": \"Button\", \"Label\": {\"Text\": \"Button\"}}]}}";
+	OctaneUI::ControlList List;
+	Application.GetMainWindow()->Load(Json, List);
+	Application.GetMainWindow()->Update();
+
+	VERIFY(List.Contains("Button"))
+
+	bool Pressed = false;
+	List.ToButton("Button")->SetOnPressed([&]()
+	{
+		Pressed = true;
+	});
+
+	OctaneUI::Vector2 Position = List.ToButton("Button")->GetAbsoluteBounds().GetCenter();
+
+	Application.GetMainWindow()->OnMouseMove(Position);
+	Application.GetMainWindow()->OnMousePressed(Position, OctaneUI::Mouse::Button::Left);
+	VERIFY(!Pressed)
+
+	Application.GetMainWindow()->OnMouseReleased(Position, OctaneUI::Mouse::Button::Left);
+	VERIFY(Pressed)
+
+	return true;
+})
+
+)
+
+}
