3bca62ea5df7aa03a3c076e782efd728335517df
mdavisprog <mdavisprog@gmail.com>
2022-02-21 14:32:19 -0800

    Clock: Add class to help measure time.
    This uses the STL high_resolution_clock class.

--- Source/OctaneUI/Clock.cpp
+++ Source/OctaneUI/Clock.cpp
@@ -24,39 +24,20 @@ SOFTWARE.
 
 */
 
-#pragma once
-
-#include "Application.h"
 #include "Clock.h"
-#include "Color.h"
-#include "Controls/Button.h"
-#include "Controls/Container.h"
-#include "Controls/Control.h"
-#include "Controls/ControlList.h"
-#include "Controls/HorizontalContainer.h"
-#include "Controls/Image.h"
-#include "Controls/ImageButton.h"
-#include "Controls/Menu.h"
-#include "Controls/MenuBar.h"
-#include "Controls/MenuItem.h"
-#include "Controls/Panel.h"
-#include "Controls/ScrollableContainer.h"
-#include "Controls/ScrollBar.h"
-#include "Controls/Text.h"
-#include "Controls/TextButton.h"
-#include "Controls/TextInput.h"
-#include "Controls/TextSelectable.h"
-#include "Controls/VerticalContainer.h"
-#include "DrawCommand.h"
-#include "Event.h"
-#include "Font.h"
-#include "Json.h"
-#include "Keyboard.h"
-#include "Mouse.h"
-#include "Paint.h"
-#include "Rect.h"
-#include "Theme.h"
-#include "Vector2.h"
-#include "Vertex.h"
-#include "VertexBuffer.h"
-#include "Window.h"
+
+namespace OctaneUI
+{
+
+Clock::Clock()
+{
+}
+
+float Clock::Measure()
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> Now = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> Diff = Now - m_Stamp;
+    return Diff.count();
+}
+
+}
