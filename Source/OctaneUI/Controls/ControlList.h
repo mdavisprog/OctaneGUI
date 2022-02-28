13a3870bca2d7f065ebeef5299bdfae7a5798493
mdavisprog <mdavisprog@gmail.com>
2022-02-27 19:13:24 -0800

    ControlList: Converted shared_ptr to weak_ptr.

--- Source/OctaneUI/Controls/ControlList.h
+++ Source/OctaneUI/Controls/ControlList.h
@@ -19,11 +19,18 @@ public:
 
 	template<class T> std::shared_ptr<T> To(const char* ID) const
 	{
-		return std::dynamic_pointer_cast<T>(m_Controls.at(ID));
+		std::shared_ptr<T> Result;
+		std::weak_ptr<Control> Item = m_Controls.at(ID);
+		if (!Item.expired())
+		{
+			Result = std::dynamic_pointer_cast<T>(Item.lock());
+		}
+
+		return Result;
 	}
 
 private:
-	typedef std::unordered_map<std::string, std::shared_ptr<Control>> Map;
+	typedef std::unordered_map<std::string, std::weak_ptr<Control>> Map;
 
 	Map m_Controls;
 };
