804528294f41da0bc05c8406a00f60a6ac7f9c4f
mdavisprog <mdavisprog@gmail.com>
2022-05-18 14:06:58 -0700

    CommandPalette: Removed SetSpacing call.
    Previously, the spacing was set to zero due to a bug where
    spacing was added after the final control in a BoxContainer.
    
    This issue has been fixed in a prior commit so it is OK to remove
    this setting.

--- Source/OctaneGUI/Tools/CommandPalette.cpp
+++ Source/OctaneGUI/Tools/CommandPalette.cpp
@@ -63,9 +63,7 @@ CommandPalette::CommandPalette(Window* InWindow)
 	Background->SetExpand(Expand::Both);
 
 	m_Container = AddControl<VerticalContainer>();
-	m_Container
-		->SetSpacing({ 0.0f, 0.0f })
-		->SetExpand(Expand::Both);
+	m_Container->SetExpand(Expand::Both);
 
 	std::shared_ptr<MarginContainer> Margins = m_Container->AddControl<MarginContainer>();
 	Margins->SetMargins({ 4.0f, 4.0f, 4.0f, 4.0f });
