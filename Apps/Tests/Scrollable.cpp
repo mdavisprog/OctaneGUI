7526be4c3d38069d9e8b7d818cb991ebd3036c82
mdavisprog <mdavisprog@gmail.com>
2022-05-19 18:56:35 -0700

    Tests: HScrollBar and VScrollBar tests.
    These tests are run to ensure that their respective scroll bars are painted by
    adding a container that extends past the width or height.

--- Apps/Tests/Scrollable.cpp
+++ Apps/Tests/Scrollable.cpp
@@ -31,12 +31,12 @@ SOFTWARE.
 namespace Tests
 {
 
-void Load(OctaneGUI::Application& Application, const char* Json, OctaneGUI::ControlList& List)
+void Load(OctaneGUI::Application& Application, const char* Json, OctaneGUI::ControlList& List, int Width = 1280, int Height = 720)
 {
 	std::string Stream = "{\"ID\": \"View\", \"Type\": \"ScrollableViewControl\", \"Expand\": \"Both\", \"Controls\": [";
 	Stream += Json;
 	Stream += "]}";
-	Utility::Load(Application, Stream.c_str(), List);
+	Utility::Load(Application, Stream.c_str(), List, Width, Height);
 }
 
 TEST_SUITE(Scrollable,
@@ -47,6 +47,30 @@ TEST_CASE(NoScrollBars,
 	Load(Application, "{}", List);
 	const std::shared_ptr<OctaneGUI::ScrollableViewControl> View = List.To<OctaneGUI::ScrollableViewControl>("View");
 	return !View->Scrollable()->HorizontalScrollBar()->ShouldPaint() && !View->Scrollable()->VerticalScrollBar()->ShouldPaint();
+})
+
+TEST_CASE(HScrollBar,
+{
+	OctaneGUI::ControlList List;
+	Load(Application, "{\"Type\": \"Container\", \"Size\": [220, 40]}", List, 200, 200);
+	const std::shared_ptr<OctaneGUI::ScrollableViewControl> View = List.To<OctaneGUI::ScrollableViewControl>("View");
+	return View->Scrollable()->HorizontalScrollBar()->ShouldPaint() && !View->Scrollable()->VerticalScrollBar()->ShouldPaint();
+})
+
+TEST_CASE(VScrollBar,
+{
+	OctaneGUI::ControlList List;
+	Load(Application, "{\"Type\": \"Container\", \"Size\": [40, 240]}", List, 200, 200);
+	const std::shared_ptr<OctaneGUI::ScrollableViewControl> View = List.To<OctaneGUI::ScrollableViewControl>("View");
+	return !View->Scrollable()->HorizontalScrollBar()->ShouldPaint() && View->Scrollable()->VerticalScrollBar()->ShouldPaint();
+})
+
+TEST_CASE(HAndVScrollBar,
+{
+	OctaneGUI::ControlList List;
+	Load(Application, "{\"Type\": \"Container\", \"Size\": [240, 240]}", List, 200, 200);
+	const std::shared_ptr<OctaneGUI::ScrollableViewControl> View = List.To<OctaneGUI::ScrollableViewControl>("View");
+	return View->Scrollable()->HorizontalScrollBar()->ShouldPaint() && View->Scrollable()->VerticalScrollBar()->ShouldPaint();
 })
 
 )
