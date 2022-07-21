a2b989a8de88255bf31cf58606a2242fd8f2270d
mdavisprog <mdavisprog@gmail.com>
2022-07-21 12:57:52 -0700

    Scripts: Add 'help' argument.
    This will print out the usage of all arguments used by the scripts.
    This commit only implements 'help' for shell scripts.

--- Scripts/Defines.sh
+++ Scripts/Defines.sh
@@ -9,6 +9,7 @@ WINDOWING=SDL2
 RENDERING=OpenGL
 CLEAN=false
 NO_APPS=OFF
+HELP=false
 
 BIN_PATH=../bin
 BUILD_PATH=../Build
@@ -29,6 +30,7 @@ do
 		ninja) NINJA=true ;;
 		clean) CLEAN=true ;;
 		noapps) NO_APPS=ON ;;
+		help) HELP=true ;;
         *) break
     esac
 done
@@ -42,4 +44,22 @@ fi
 
 if [ "$NINJA" = true ] ; then
 	GENERATOR=Ninja
+fi
+
+if [ "$HELP" = true ] ; then
+	echo "This is a list of arguments and a description for each:"
+	echo "debug          Sets the configuration to Debug. The default is Release."
+	echo "tools          Compiles with tools enabled. This can be compiled in any configuration."
+	echo "ninja          Use the ninja build system instead of the default (make). The path"
+	echo "               to the ninja build system must be added to the PATH environment variable."
+	echo "clean          Cleans the intermediate files before generating and building. This forces"
+	echo "               a full rebuild of the project."
+	echo "noapps         Only compiles the library."
+	echo "sfml           Builds the apps using the SFML library. The SFML_DIR variable must be set"
+	echo "               for the generator to locate the library."
+	echo "sdl2           Builds the app using the SDL library. The SDL2 cmake and library paths must"
+	echo "               be locatable by the generator through either the environment variables or the "
+	echo "               SDL2_DIR and SDL2_MODULE_PATH variables."
+	echo "help           Displays this help message."
+	exit 0
 fi
