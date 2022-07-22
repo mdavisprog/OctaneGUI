f351f1036b675a7c8bd7b0e8c911d1fb930fa387
mdavisprog <mdavisprog@gmail.com>
2022-07-22 15:08:16 -0700

    Scripts: Add 'help' argument for batch files.

--- Scripts/Defines.bat
+++ Scripts/Defines.bat
@@ -9,6 +9,7 @@ SET CLEAN=FALSE
 SET WINDOWING=SFML
 SET RENDERING=SFML
 SET NO_APPS=OFF
+SET HELP=FALSE
 
 SET BIN_PATH=..\bin
 SET BUILD_PATH=..\Build
@@ -23,6 +24,7 @@ IF NOT "%1" == "" (
 	IF /I "%1" == "NINJA" SET NINJA=TRUE
 	IF /I "%1" == "CLEAN" SET CLEAN=TRUE
 	IF /I "%1" == "NOAPPS" SET NO_APPS=ON
+	IF /I "%1" == "HELP" SET HELP=TRUE
 	SHIFT
 	GOTO :PARSE_ARGS
 )
@@ -41,4 +43,22 @@ IF "%SFML%" == "TRUE" (
 
 IF "%NINJA%" == "TRUE" (
 	SET GENERATOR=Ninja
+)
+
+IF "%HELP%" == "TRUE" (
+	ECHO This is a list of arguments and a description for each:
+	ECHO debug          Sets the configuration to Debug. The default is Release.
+	ECHO tools          Compiles with tools enabled. This can be compiled in any configuration.
+	ECHO ninja          Use the ninja build system instead of the default (msbuild^). The path
+	ECHO                to the ninja build system must be added to the PATH environment variable.
+	ECHO clean          Cleans the intermediate files before generating and building. This forces
+	ECHO                a full rebuild of the project.
+	ECHO noapps         Only compiles the library.
+	ECHO sfml           Builds the apps using the SFML library. The SFML_DIR variable must be set
+	ECHO                for the generator to locate the library.
+	ECHO sdl2           Builds the app using the SDL library. The SDL2 cmake and library paths must
+	ECHO                be locatable by the generator through either the environment variables or the 
+	ECHO                SDL2_DIR and SDL2_MODULE_PATH variables.
+	ECHO help           Displays this help message.
+	EXIT 0
 )
