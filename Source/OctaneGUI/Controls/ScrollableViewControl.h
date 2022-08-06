774fc0cfecff910b1a69c1d26c85040ca4b99e53
mdavisprog <mdavisprog@gmail.com>
2022-08-05 17:06:58 -0700

    ScrollableViewControl: Make functions protected.
    Make AddControl, InsertControl, and RemoveControl protected to prevent
    attempts to add controls to this container. All controls should be added to
    the Scrollable container.

--- Source/OctaneGUI/Controls/ScrollableViewControl.h
+++ Source/OctaneGUI/Controls/ScrollableViewControl.h
@@ -76,6 +76,11 @@ public:
     virtual bool OnMousePressed(const Vector2& Position, Mouse::Button Button, Mouse::Count Count) override;
     virtual void OnMouseReleased(const Vector2& Position, Mouse::Button Button) override;
 
+protected:
+    using Container::AddControl;
+    using Container::InsertControl;
+    using Container::RemoveControl;
+
 private:
     bool AlwaysFocusInteraction() const;
 
