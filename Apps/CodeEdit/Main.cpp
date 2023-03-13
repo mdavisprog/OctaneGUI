372c0a8b4339b58cd04ecd61973e05c09ff39272
mdavisprog <mdavisprog@gmail.com>
2023-03-13 16:35:02 -0700

    CodeEdit: Add TextEditor control with File options.
    The File -> Open option will load the file contents into the TextEditor control.

--- Apps/CodeEdit/Main.cpp
+++ Apps/CodeEdit/Main.cpp
@@ -11,5 +11,25 @@ int main(int, char**)
     std::unordered_map<std::string, OctaneGUI::ControlList> WindowControls;
     Application.Initialize(Contents.c_str(), WindowControls);
 
+    const OctaneGUI::ControlList& ControlList = WindowControls["Main"];
+    std::shared_ptr<OctaneGUI::TextEditor> Editor = ControlList.To<OctaneGUI::TextEditor>("Editor");
+
+    Application.FS().SetOnFileDialogResult([&](OctaneGUI::FileDialogType Type, const std::u32string& FileName) -> void
+        {
+            if (Type == OctaneGUI::FileDialogType::Open)
+            {
+                Editor->OpenFile(FileName.c_str());
+            }
+        });
+
+    ControlList.To<OctaneGUI::MenuItem>("File.Open")->SetOnPressed([&](OctaneGUI::TextSelectable&) -> void
+        {
+            Application.FS().FileDialog(OctaneGUI::FileDialogType::Open);
+        });
+    ControlList.To<OctaneGUI::MenuItem>("File.Quit")->SetOnPressed([&](OctaneGUI::TextSelectable&) -> void
+        {
+            Application.Quit();
+        });
+
     return Application.Run();
 }
