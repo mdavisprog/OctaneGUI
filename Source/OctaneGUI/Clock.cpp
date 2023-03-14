598b914e6f13634f25d8c2a2fe34e0a483a57768
mdavisprog <mdavisprog@gmail.com>
2023-03-13 17:58:23 -0700

    Library: Update copyright years.

--- Source/OctaneGUI/Clock.cpp
+++ Source/OctaneGUI/Clock.cpp
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2022 Mitchell Davis <mdavisprog@gmail.com>
+Copyright (c) 2022-2023 Mitchell Davis <mdavisprog@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
